dd478ba0ce5b70b7fbda8fe9e34d889a38100557
CrackerTeg <crackerteg@gmail.com>
2016-11-05 20:42:08 -0500

    Brokededed it
    Signed-off-by: CrackerTeg <crackerteg@gmail.com>

--- broken.mk
+++ broken.mk
@@ -17,10 +17,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from device.
+$(call inherit-product, device/leeco/x2/device.mk)
 
-PRODUCT_NAME := cm_x2
+# Inherit some common stuff.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+
+PRODUCT_NAME := broken_x2
 BOARD_VENDOR := leeco
 TARGET_VENDOR := leeco
 PRODUCT_DEVICE := x2
